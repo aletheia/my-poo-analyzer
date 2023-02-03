daba2e844dba044410070db9afc3455a3abcd2e9
Luca Bianchi <bianchi.luca@gmail.com>
2023-02-03 14:55:09 +0100

    :sparkles: Update with dataset generation and data analysis

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import streamlit as st
 
-from utils import convert, plot_medicine, plot_by_type, type_to_number, number_to_type
+from utils import convert, plot_by_drug, plot_by_type, type_to_number, number_to_type, type_bucketize, get_unique_strings, figure_to_be_plotted, plot_by_values
 
 csv_path = './export.csv'
 
@@ -16,28 +16,90 @@ st.title('My Poo analyzer')
 # Convert and normalize dataset
 df['Date'] = df['Date'].apply(convert)
 df = df.rename(columns={'Farmaci (da <12h)': 'Farmaci',
-               'Variazioni nella dieta (ultime 24h)': 'Variazioni'})
-
+               'Variazioni nella dieta (ultime 24h)': 'Variazioni',
+                        'Altri sintomi': 'Sintomi',
+                        })
 dd = df.copy()
 dd = dd.dropna(how='all')
+dd = dd.drop(columns=['Nastriformi?'], axis=1)
 dd['Tipo'] = dd['Tipo'].apply(lambda x: type_to_number(x))
 dd = dd[dd['Tipo'] > 0]
 
+# Build unique drugs list
+drug_list = dd['Farmaci'].unique().tolist()
+drug_list = get_unique_strings(drug_list)
+drug_list = [x for x in drug_list if (x != 'nan') & (x != '<NA>') & (x != '')]
+drug_list.sort()
+
+# Build unique diet list
+diet_list = dd['Variazioni'].unique().tolist()
+diet_list = get_unique_strings(diet_list)
+diet_list = [x for x in diet_list if (x != 'nan') & (x != '<NA>') & (x != '')]
+diet_list.sort()
+
+# Build unique symptoms list
+symptoms_list = dd['Sintomi'].unique().tolist()
+symptoms_list = get_unique_strings(symptoms_list)
+symptoms_list = [x for x in symptoms_list if (
+    x != 'nan') & (x != '<NA>') & (x != '')]
+symptoms_list.sort()
 
 # Sidebar
-start_date = st.sidebar.date_input('Start date', value=df['Date'].min())
-end_date = st.sidebar.date_input('End date', value=df['Date'].max())
+st.sidebar.markdown('## Filtri')
+st.sidebar.markdown('### Date')
+start_date = st.sidebar.date_input('Data di inizio', value=df['Date'].min())
+end_date = st.sidebar.date_input('Data di fine', value=df['Date'].max())
+
+st.sidebar.markdown('---')
+st.sidebar.markdown('### Tipo')
 type_range = st.sidebar.slider('Tipo - Range normalità', 0, 7, (3, 5))
-st.sidebar.write(f'Stitichezza: tipo < {type_range[0]}')
-st.sidebar.write(
-    f'Normale: tipo >= {type_range[0]} e =< {type_range[1]}')
-st.sidebar.write(f'Diarrea: tipo > {type_range[1]}')
+st.sidebar.markdown(
+    f'##### Stitichezza: tipo < {type_range[0]}  ')
+st.sidebar.markdown(
+    f'##### Normale: tipo >= {type_range[0]} e =< {type_range[1]} ')
+st.sidebar.markdown(f'##### Diarrea: tipo > {type_range[1]}')
+
+st.sidebar.markdown('---')
+st.sidebar.markdown('#### Farmaci assunti')
+selected_drug = st.sidebar.multiselect('Farmaco', drug_list)
+st.sidebar.markdown('#### Variazioni nella dieta')
+selected_diet = st.sidebar.multiselect('Variazione', diet_list)
+st.sidebar.markdown('#### Sintomi')
+selected_symptoms = st.sidebar.multiselect('Sintomo', symptoms_list)
+
 
 # Filter dataset by date
 dd = dd[dd['Date'] >= pd.to_datetime(start_date)]
 dd = dd[dd['Date'] <= pd.to_datetime(end_date)]
 
-st.write(f'Analisi dei dati dal {start_date} al {end_date}')
+dd['BucketedType'] = dd['Tipo'].apply(
+    type_bucketize, args=(type_range[0], type_range[1]))
+
+st.markdown(
+    f'Analisi dei dati dal **{start_date.strftime("%d/%m/%Y")}** al **{end_date.strftime("%d/%m/%Y")}**')
+
+
+dataset = dd.copy()
+
+
+def fi(x, value):
+    try:
+        return x.find(value) > -1
+    except:
+        return False
+
+
+for drug in drug_list:
+    dataset[drug] = dataset['Farmaci'].apply(fi, args=(drug,))
+
+for diet in diet_list:
+    dataset[diet] = dataset['Variazioni'].apply(fi, args=(diet,))
+
+for symptom in symptoms_list:
+    dataset[symptom] = dataset['Sintomi'].apply(fi, args=(symptom,))
+
+dataset = dataset[dataset['Date'] >= pd.to_datetime(start_date)]
+dataset = dataset[dataset['Date'] <= pd.to_datetime(end_date)]
 
 
 st.markdown('---')
@@ -55,22 +117,9 @@ gplot = grouped_by_type.plot(kind='barh')
 st.pyplot(gplot.figure)
 
 st.write('Rappresentazione del dataset in base relativamente al tipo (secondo la scala di Bristol) come classificato, raggruppato per date, conteggiando il numero di eventi di una determinata tipologia')
-
 bucketed_by_type = dd.copy()
-
-
-def classify(x):
-    if x < type_range[0]:
-        return 'Stitichezza'
-    elif x <= type_range[1]:
-        return 'Normale'
-    else:
-        return 'Diarrea'
-
-
-bucketed_by_type['Tipo'] = bucketed_by_type['Tipo'].apply(classify)
-bucketed_by_type = bucketed_by_type.groupby('Tipo').count()
-bucketed_by_type = bucketed_by_type[['Date']]
+bucketed_by_type = bucketed_by_type[['Date', 'BucketedType']]
+bucketed_by_type = bucketed_by_type.groupby('BucketedType').count()
 bplot = bucketed_by_type.plot(kind='pie', subplots=True, figsize=(10, 10))
 st.pyplot(bplot[0].figure)
 
@@ -106,29 +155,112 @@ st.write(f'La media di evacuazioni giornaliere è {fc["Tipo"].mean()}.\n'
          f'Giornalmente è più frequente il caso con mediana {fc["Tipo"].median()} evacuazioni'
          f'e moda {fc["Tipo"].mode()[0]} evacuazioni')
 
+
+st.markdown('---')
+st.markdown('---')
+st.markdown('## Analisi in base al farmaco assunto')
+
+top_data_container = st.container()
+with top_data_container:
+    left, right = st.columns(2)
+    with left:
+        st.markdown(
+            f'**Farmaci assunti nel periodo {start_date} - {end_date}**')
+        st.pyplot(plot_by_values(dataset, drug_list))
+    with right:
+        st.markdown(
+            f'**Feci normali nel periodo {start_date} - {end_date} in relazione all\'assunzione di farmaci**')
+        pl = plot_by_values(
+            dataset[dataset['BucketedType'] == 'Normale'], drug_list)
+        if (pl is not None):
+            st.pyplot(pl)
+        else:
+            st.markdown('Nessun dato nel periodo selezionato')
+
+st.markdown('---')
+bottom_data_container = st.container()
+with bottom_data_container:
+
+    left, right = st.columns(2)
+    with left:
+        st.markdown(
+            f'**Feci stitiche nel periodo {start_date} - {end_date} in relazione all\'assunzione di farmaci**')
+        pl = plot_by_values(
+            dataset[dataset['BucketedType'] == 'Stitichezza'], drug_list)
+        if (pl is not None):
+            st.pyplot(pl)
+        else:
+            st.markdown('Nessun dato nel periodo selezionato')
+
+    with right:
+        st.markdown(
+            f'**Feci diarroiche nel periodo {start_date} - {end_date} in relazione all\'assunzione di farmaci**')
+        pl = plot_by_values(
+            dataset[dataset['BucketedType'] == 'Diarrea'], drug_list)
+        if (pl is not None):
+            st.pyplot(pl)
+        else:
+            st.markdown('Nessun dato nel periodo selezionato')
+
+
+if len(selected_drug) > 0:
+    st.markdown(
+        f'Classificazione dei dati in base all\'assuzione di **{selected_drug}**')
+else:
+    st.markdown(
+        f'Selezionare almeno un farmaco per visualizzare i dati')
+
+rf = dataset.copy()
+if (len(selected_drug) > 0):
+    for drug in selected_drug:
+        rf = rf[rf[drug]]
+    drug_plot = figure_to_be_plotted(rf)
+    st.pyplot(drug_plot.figure)
+
+st.markdown('---')
+st.markdown('## Analisi in base alle variazioni nella dieta')
+st.pyplot(plot_by_values(dataset, diet_list))
+
+if len(selected_diet) > 0:
+    st.markdown(
+        f'Classificazione dei dati in base ad una dieta di tipo **{selected_diet}**')
+else:
+    st.markdown(
+        f'Selezionare almeno un regime alimentare per visualizzare i dati')
+
+rf = dataset.copy()
+if (len(selected_diet) > 0):
+    for diet in selected_diet:
+        rf = rf[rf[diet]]
+    if len(rf) > 0:
+        diet_plot = figure_to_be_plotted(rf)
+        st.pyplot(diet_plot.figure)
+    else:
+        st.markdown('##### Nessun dato trovato')
+
 st.markdown('---')
 
-st.markdown('## Analisi in base al farmaco assunto')
-st.write('Classificazione dei dati in base al farmaco assunto')
-st.write('### Farmaci assunti')
+st.markdown('## Analisi in base ai sintomi')
+st.pyplot(plot_by_values(dataset, symptoms_list))
 
 
-def get_unique_strings(input_list):
-    split_strings = []
-    for string in input_list:
-        chunks = []
-        for chunk in str(string).split(','):
-            chunks.append(chunk.strip())
-        split_strings += chunks
-    return list(set(split_strings))
+if len(selected_symptoms) > 0:
+    st.markdown(
+        f'Classificazione dei dati in base ai sintomi **{selected_symptoms}**')
+else:
+    st.markdown(
+        f'Selezionare almeno un sintomo per visualizzare i dati')
 
+rf = dataset.copy()
+if (len(selected_symptoms) > 0):
+    for symton in selected_symptoms:
+        rf = rf[rf[symton]]
+    if len(rf) > 0:
+        symton_plot = figure_to_be_plotted(rf)
+        st.pyplot(symton_plot.figure)
+    else:
+        st.markdown('##### Nessun dato trovato')
 
-dlist = df['Farmaci'].unique().tolist()
-drugs = get_unique_strings(dlist)
-drugs.sort()
-# drugs = list(set(''.join(' ').split(',')))
-st.sidebar.write('Farmaci assunti')
-selected_drug = st.sidebar.selectbox('Farmaco', drugs)
-
-drug_plot = plot_medicine(df, selected_drug)
-st.pyplot(drug_plot.figure)
+st.markdown('---')
+st.markdown('## Raw data')
+st.dataframe(dataset)
--- src/utils.py
+++ src/utils.py
@@ -14,7 +14,7 @@ def convert(x):
             return pd.to_datetime(x, format='%B %d, %Y')
 
 
-def plot_medicine(df, medicine, x_interval=2):
+def plot_by_drug(df, medicine, x_interval=2):
     fa = df.copy()
     fa['Farmaci'] = fa['Farmaci'].dropna()
 
@@ -31,15 +31,11 @@ def plot_medicine(df, medicine, x_interval=2):
     dfa = dfa['Tipo']
     dfa = dfa.reset_index()
     dfa['Tipo'] = dfa['Tipo'].apply(lambda x: type_to_number(x))
-    fig = dfa.plot(x='Date', y='Tipo', kind='scatter', figsize=(16, 9))
+    fig = dfa.plot(x='Date', y='Tipo', kind='scatter', figsize=(16, 9), rot=45)
     fig.axhline(y=2.5, color='g', linestyle='-')
     fig.axhline(y=4.5, color='g', linestyle='-')
     fig.xaxis.set_major_locator(mdates.DayLocator(interval=x_interval))
     fig.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%Y'))
-
-    # plt.gcf().autofmt_xdate()
-    # ax.ylim([1, 7])
-
     grouped_by_type = dfa
     grouped_by_type['Tipo'] = grouped_by_type['Tipo'].apply(
         lambda x: number_to_type(x))
@@ -49,12 +45,27 @@ def plot_medicine(df, medicine, x_interval=2):
     return fig
 
 
-def plot_by_type(df):
+def figure_to_be_plotted(df, x_interval=2):
+    dfa = df.copy()
+    dfa = dfa.set_index('Date')
+    dfa = dfa['Tipo']
+    dfa = dfa.reset_index()
+    #dfa['Tipo'] = dfa['Tipo'].apply(lambda x: type_to_number(x))
+    fig = dfa.plot(x='Date', y='Tipo', kind='scatter', figsize=(16, 9), rot=45)
+    fig.axhline(y=2.5, color='g', linestyle='-')
+    fig.axhline(y=4.5, color='g', linestyle='-')
+    fig.xaxis.set_major_locator(mdates.DayLocator(interval=x_interval))
+    fig.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%Y'))
+    return fig
+
+
+def plot_by_type(df, x_interval=2):
     rr = df[['Date', 'Tipo']]
-    tplot = rr.plot(x='Date', y='Tipo', kind='scatter', figsize=(26, 9))
+    tplot = rr.plot(x='Date', y='Tipo', kind='scatter',
+                    figsize=(26, 9), rot=45)
     tplot.axhline(y=2.5, color='g', linestyle='-')
     tplot.axhline(y=4.5, color='g', linestyle='-')
-    tplot.xaxis.set_major_locator(mdates.DayLocator(interval=1))
+    tplot.xaxis.set_major_locator(mdates.DayLocator(interval=x_interval))
     tplot.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m'))
     # tplot.gcf().autofmt_xdate()
     return tplot
@@ -86,3 +97,37 @@ def number_to_type(x):
         0: 'Non determinato'
     }
     return number_to_type[x]
+
+
+def type_bucketize(x, min, max):
+    if x < min:
+        return 'Stitichezza'
+    elif x <= max:
+        return 'Normale'
+    else:
+        return 'Diarrea'
+
+
+def get_unique_strings(input_list):
+    split_strings = []
+    for string in input_list:
+        chunks = []
+        for chunk in str(string).split(','):
+            chunks.append(chunk.strip())
+        split_strings += chunks
+    return list(set(split_strings))
+
+
+def plot_by_values(df, values, figsize=(16, 9)):
+    if (len(values) > 0 and len(df) > 0):
+        counter = dict()
+        for symptom in values:
+            s = df[symptom].value_counts()
+            counter[symptom] = s.to_dict()[True] if True in s else 0
+
+        plt_dataframe = pd.DataFrame.from_dict(counter, orient='index')
+        pie_plot = plt_dataframe.plot(
+            kind='pie', figsize=figsize, subplots=True)
+        return pie_plot[0].figure
+    else:
+        return None
